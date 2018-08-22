bd3fb12d7f9dcc9c69dd2ae1772eead1c90ba92e
nicklasoxhammar <nicklas.oxhammar@hotmail.com>
2018-08-22 09:24:09 +0200

    Made a player that can move and added the 'terrain'

---
+++ Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move();
+		
+	}
+
+    void Move() {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+
+            if (transform.position.x > -4) {
+                transform.position += Vector3.left * 4;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+
+            if (transform.position.x < 4) {
+                transform.position += Vector3.right * 4;
+            }
+        }
+
+
+    }
+
+   
+}
